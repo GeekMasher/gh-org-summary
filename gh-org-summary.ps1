da659e6d2370c91e47b1c3c1f84252c7e841c602
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2023-12-19 16:06:54 +0000

    Fix extension path

--- gh-org-summary.ps1
+++ gh-org-summary.ps1
@@ -1,8 +1,11 @@
 
+# Get the directory of this script
+$ExtensionPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 # if first argument is 'install'
 if ($args[0] -eq 'install') {
     pip install ghastoolkit
     exit
 }
 
-python3 "$PSScriptRoot/summary.py" $args
+python3 "$ExtensionPath/summary.py" $args
